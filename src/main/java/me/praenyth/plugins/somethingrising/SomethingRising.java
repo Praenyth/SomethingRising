b1e2f19d145781bafca13ee47ac9f80b99f35e30
Praenyth <praenyth@gmail.com>
2022-12-19 20:03:47 -0500

    using incendo cloud for commands :3

--- src/main/java/me/praenyth/plugins/somethingrising/SomethingRising.java
+++ src/main/java/me/praenyth/plugins/somethingrising/SomethingRising.java
@@ -1,7 +1,17 @@
 package me.praenyth.plugins.somethingrising;
 
+import cloud.commandframework.CommandManager;
+import cloud.commandframework.annotations.AnnotationParser;
+import cloud.commandframework.bukkit.BukkitCommandManager;
+import cloud.commandframework.execution.CommandExecutionCoordinator;
+import cloud.commandframework.meta.SimpleCommandMeta;
 import me.praenyth.plugins.somethingrising.commands.RisingCommand;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.units.qual.C;
+
+import java.util.function.Function;
 
 public final class SomethingRising extends JavaPlugin {
 
@@ -10,11 +20,15 @@ public final class SomethingRising extends JavaPlugin {
     @Override
     public void onEnable() {
 
-        // register commands
         try {
-
-        } catch (final Exception e) {
-            getLogger().warning("Failed to initialize Brigadier support: " + e.getMessage());
+            CommandManager<CommandSender> manager = new BukkitCommandManager<>(
+                    /* Owning plugin */ this,
+                    CommandExecutionCoordinator.simpleCoordinator(),
+                    Function.identity(),
+                    Function.identity()
+            );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
         getLogger().info("Hopefully something can rise out of the ashes and kill players!");
